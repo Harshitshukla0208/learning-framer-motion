1f04fc9d9cc615c83930aaf3239b18f2508c7cb9
Harshit Shukla <mrharshit2409@gmail.com>
2024-08-02 18:20:52 +0530

    l-2

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 import Navbar from "../components/Navbar"
 export default function Home() {
   return (
-    <div className="relative">
-      <Navbar />
+    <div>
+      
     </div>
   );
 }
---
+++ src/components/BasicsOfMotion.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const BasicsOfMotion = () => {
+    return (
+        <div
+        style={{
+            display: "grid",
+            placeContent: "center",
+            height: "100vh",
+            gap: "0.8rem",
+        }}>
+            <div style={{
+                width: "150",
+                height: "150",
+                background: "black"
+            }}>
+
+            </div>
+        </div>
+    )
+}
+
+export default BasicsOfMotion
