74b3198b783eb0d935f672752a3ccaeec942c995
Harshit Shukla <mrharshit2409@gmail.com>
2024-08-06 16:22:56 +0530

    l-7

--- src/components/ViewBasedAnimations.jsx
+++ src/components/ViewBasedAnimations.jsx
@@ -1,15 +1,32 @@
 "use client"
-import React from 'react'
-import {motion} from 'framer-motion'
+import React, { useEffect, useRef } from 'react'
+import {motion, useInView} from 'framer-motion'
 
 const ViewBasedAnimations = () => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once:true }); //this once:true property allows the animations to fire only single time, its not gona be firing repeatedly
+
+    useEffect(() => {
+        console.log("Is in view -> ", isInView)
+    },[isInView])
+
     return (
         <>
+            {/* method 1 */}
             <div className='h-[150vh]' />
             <motion.div className='bg-black h-screen'
                 initial={{opacity: 0}}
                 whileInView={{opacity: 1}}
                 transition={{duration: 1}}
+            />
+
+            {/* method 2 */}
+            <div 
+                ref={ref}
+                style={{height: "100vh",
+                    background: isInView ? "blue" : "red",
+                    transition: "1s background",
+                }}
             />
         </>
     )
